194ce38907c16db37baf55256783b0dd008508ba
realdogdad <brandan@getfoster.net>
2022-09-08 21:29:14 -0700

    completed tutorial

--- web/script.js
+++ web/script.js
@@ -1,7 +1,7 @@
 //start of get request to retrieve available coffees
 
 const getInventory = async () => {
-  let results = await axios.get('/api/cloud/getCoffee');
+  let results = await axios.get('https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-a178602b-db5e-449c-9212-5be181ff7c01/cloud/getCoffee');
 results.data.forEach(item => {
   let pic = item.pic;
   let name = item.name;
@@ -73,7 +73,7 @@ const cartHandler = function() {
 let subscribeButton = document.getElementById("subscribe");
 const subscribeHandler = async function() {
   let email = document.getElementById("email").value
-  let emailUrl = "https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-a178602b-db5e-449c-9212-5be181ff7c01/cloud/post-email" + "?email=" + email;
+  let emailUrl = "https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-a178602b-db5e-449c-9212-5be181ff7c01/cloud/postEmail" + "?email=" + email;
   await axios.post(emailUrl);
   localStorage.setItem("subscribe", email);
   document.getElementById("email").value = '';
